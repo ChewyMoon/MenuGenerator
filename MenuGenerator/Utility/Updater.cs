5ae87eb22e5a9cf3d1d43cd2fae37f05eaf39082
Justin Perez <chewymoonleaguesharp@gmail.com>
2016-01-12 21:38:31 -0500

    Better updater

--- MenuGenerator/Utility/Updater.cs
+++ MenuGenerator/Utility/Updater.cs
@@ -1,10 +1,12 @@
 ﻿namespace MenuGenerator.Utility
 {
     using System;
+    using System.Diagnostics;
     using System.IO;
     using System.Net;
     using System.Reflection;
     using System.Text.RegularExpressions;
+    using System.Threading.Tasks;
     using System.Windows;
 
     /// <summary>
@@ -19,35 +21,50 @@
         /// </summary>
         public static void CheckForUpdates()
         {
-            var request =
-                    WebRequest.Create(
-                        "https://github.com/ChewyMoon/MenuGenerator/blob/master/MenuGenerator/Properties/AssemblyInfo.cs");
-            var response = request.GetResponse();
-            var data = response.GetResponseStream();
+            Task.Run(
+                () =>
+                    {
+                        var request =
+                            WebRequest.Create(
+                                "https://raw.githubusercontent.com/ChewyMoon/MenuGenerator/master/MenuGenerator/Properties/AssemblyInfo.cs");
+                        var response = request.GetResponse();
+                        var data = response.GetResponseStream();
 
-            string version = null;
+                        string version = null;
 
-            if (data != null)
-            {
-                using (var sr = new StreamReader(data))
-                {
-                    version = sr.ReadToEnd();
-                }
-            }
+                        if (data != null)
+                        {
+                            using (var sr = new StreamReader(data))
+                            {
+                                version = sr.ReadToEnd();
+                            }
+                        }
 
-            const string Pattern = @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}";
+                        const string Pattern = @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}";
 
-            if (version == null)
-            {
-                return;
-            }
+                        if (version == null)
+                        {
+                            return;
+                        }
 
-            var serverVersion = new Version(new Regex(Pattern).Match(version).Groups[0].Value);
+                        var serverVersion = new Version(new Regex(Pattern).Match(version).Groups[0].Value);
 
-            if (serverVersion > Assembly.GetExecutingAssembly().GetName().Version)
-            {
-                MessageBox.Show("There is a new version avaiable! Download at github.", "Update Available");
-            }
+                        if (serverVersion <= Assembly.GetExecutingAssembly().GetName().Version)
+                        {
+                            return;
+                        }
+
+                        var result = MessageBox.Show(
+                            "An update is available! Would you like to download it?",
+                            "Update Available",
+                            MessageBoxButton.YesNo,
+                            MessageBoxImage.Information);
+
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            Process.Start("https://github.com/ChewyMoon/MenuGenerator/releases");
+                        }
+                    });
         }
 
         #endregion
